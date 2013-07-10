e455460ef9fc974e93893196b91916b789a09bc4
Jan Stolarek <jan.stolarek@p.lodz.pl>
2013-07-10 15:42:00 +0100

    Duplicate entry block instead of loopifying

--- compiler/cmm/CmmLoopify.hs
+++ compiler/cmm/CmmLoopify.hs
@@ -8,15 +8,21 @@ import CLabel
 import CmmUtils
 import Hoopl
 
--- This module implements loopification optimisation as described in
--- "Low-level code optimisations in the Glasgow Haskell Compiler" by
--- Krzysztof Wos. The idea is to take a CmmDecl - which is either data
--- section declaration or a procedure, but loopification only works
--- on procedures - and extract label of its info table. We also extract
--- label of the entry block (first block in a graph). Then we examine all
--- blocks in a procedure graph and whenever we encounter a tail-call
--- to procedure's info table we replace it with an uncoditional jump (goto)
--- to the label of the entry block.
+-- This module implements loopification and copying of the first basic
+-- block of a loop as described in "Low-level code optimisations in the
+-- Glasgow Haskell Compiler" by Krzysztof Wos. The idea is to take a CmmDecl
+-- - which is either data section declaration or a procedure, but loopification
+-- only works on procedures - and extract label of its info table. We also
+-- extract label of the entry block (first block in a graph). Then we examine
+-- all blocks in a procedure graph and whenever we encounter a tail-call
+-- to procedure's info table we replace that call with a copy of the entry
+-- block in the procedure. There are two consequences of this:
+--    1. The block no longer does a tail-call. We instead have a goto, which
+--       makes the loop more explicit
+--    2. entry block of a procedure often loads parameters passed through the
+--       stack into registers. By copying that code to the end of loop we
+--       might have an opportunity for further optimisations, e.g. we may be
+--       able to avoid using the stack by keeping all variables in registers
 
 cmmLoopify :: CmmDecl -> CmmGraph -> CmmGraph
 cmmLoopify proc graph
@@ -24,16 +30,17 @@ cmmLoopify proc graph
               = ofBlockList (g_entry graph) $ map (loopify nfo_lbl) blocks
    | otherwise = graph
   where
-    blocks = toBlockListEntryFirst graph
-    e_lbl  = entryLabel (head blocks)
+    blocks         = toBlockListEntryFirst graph
+    (_, entry_blk) = blockSplitHead $ head blocks
 
     loopify :: CLabel -> CmmBlock -> CmmBlock
-    loopify nfo_lbl = mapBlock rw_node
-      where
-        rw_node :: CmmNode e x -> CmmNode e x -- Note [Pre-CPS Loopification]
-        rw_node (CmmCall (CmmLit (CmmLabel lbl)) Nothing _ _ _ _)
-          | lbl == nfo_lbl = CmmBranch e_lbl
-        rw_node n = n
+    loopify nfo_lbl block = rw_node exit_node
+        where
+          (blk_head, exit_node) = blockSplitTail block
+          rw_node :: CmmNode O C -> CmmBlock
+          rw_node (CmmCall (CmmLit (CmmLabel lbl)) Nothing _ _ _ _)
+            | lbl == nfo_lbl = blockAppend blk_head entry_blk
+          rw_node _ = block
 
 {- Note [Pre-CPS Loopification]
 
