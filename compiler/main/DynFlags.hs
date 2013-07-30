71bd29fc63fca14ec8bd325fef949875c17c140c
Jan Stolarek <jan.stolarek@p.lodz.pl>
2013-07-30 14:35:09 +0100

    Change facts representation, new analysis

--- compiler/main/DynFlags.hs
+++ compiler/main/DynFlags.hs
@@ -2837,8 +2837,8 @@ optLevelFlags
 --    , ([2],     Opt_RegsGraph)
     , ([0,1,2], Opt_LlvmTBAA)
     , ([1,2],   Opt_CmmSink)
-    , ([1,2],   Opt_CmmLoopify)
-    , ([1,2],   Opt_CmmCopyPropagation)
+--    , ([1,2],   Opt_CmmLoopify)
+--    , ([1,2],   Opt_CmmCopyPropagation)
     , ([1,2],   Opt_CmmElimCommonBlocks)
 
 --     , ([2],     Opt_StaticArgumentTransformation)
