bdbb66fc8c50c5da1bd76e653239e795c5959cb9
Jan Stolarek <jan.stolarek@p.lodz.pl>
2013-08-13 11:07:27 +0100

    Update comments

--- compiler/cmm/CmmCopyPropagation.hs
+++ compiler/cmm/CmmCopyPropagation.hs
@@ -22,18 +22,13 @@ import PprCmm
 import Debug.Trace
 
 -- A TODO and FIXME list for this module:
---  * use cmmMachOpFold from CmmOpt to do constant folding after rewriting
---  * I'm not sure about passing [] to dataFlowPassFwd. Perhaps I should pass
---    something else?
+--  * use cmmMachOpFold from CmmOpt to do constant folding after rewriting?
 --  * there is some naming confusion with fields of function calls. One of
 --    the fields is called `actual` but the comment mentions these are the
 --    arguments. Which name is better to use? related: rwActual and some notes
 --    There is a similar problem witg results vs. formals (see rwResults).
 --    related: [Rewriting function calls]
 --  * move sumChanges to some utility module
---  * CmmCall, CmmForeignCall and CmmUnsafeForeignCall erase all facts, which
---    is too conservative. They should only erase registers that may be
---    modified by the call
 
 -----------------------------------------------------------------------------
 --                           Copy propagation pass
@@ -42,10 +37,6 @@ import Debug.Trace
 cmmCopyPropagation :: DynFlags -> CmmGraph -> UniqSM CmmGraph
 cmmCopyPropagation dflags graph = do
      let entry_blk      = g_entry graph
-       {-  liveness       = cmmGlobalLiveness dflags $ graph
-         entryLiveness  = expectJust "entry to copy propagation" $ mapLookup entry_blk liveness
-         liveGlobalRegs = map (\r -> (CmmGlobal r, CpTop)) . S.toList $ entryLiveness
-         entryRegFacts  = M.fromList liveGlobalRegs-}
      g' <- dataflowPassFwd graph [(entry_blk, emptyCpFacts)] $
            analRewFwd cpLattice cpTransfer (cpRewrite dflags)
      return . fst $ g'
@@ -56,14 +47,14 @@ cmmCopyPropagation dflags graph = do
 
 type RegisterLocation    = CmmReg
 type StackLocation       = (Area, Int)
-type CpFact              = CmmExpr -- See Note [Assignment facts lattice]
+type CpFact              = CmmExpr
 type AssignmentFact  a   = M.Map a CpFact
-data AssignmentFactBot a = Bottom
+data AssignmentFactBot a = Bottom -- See Note [Assignment facts lattice]
                          | Const (AssignmentFact a)
                            deriving (Eq)
 type RegisterFacts       = AssignmentFactBot RegisterLocation
 type StackFacts          = AssignmentFactBot StackLocation
-type CpFacts             = (StackFacts, RegisterFacts)
+type CpFacts             = (StackFacts, RegisterFacts) -- See Note [Copy propagation facts]
 
 emptyCpFacts :: CpFacts
 emptyCpFacts = (Const M.empty, Const M.empty)
@@ -89,15 +80,15 @@ instance (Show a) => Show (AssignmentFactBot a) where
 -- Note [Copy propagation facts]
 -- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 --
--- We distinguish two kinds of facts. First are the assignments to registers,
--- which are refered to by a value of CmmReg. Second are the assignments to
--- stack locations, which are represented by a tuple (Area, Int). Since this
--- pass is done before the stack layout, we don't yet have explict global
--- registers like Sp and therefore we operate on stack areas with an offset
--- (these are stored in a CmmStackSlot data constructor). Both types of facts
--- take the same values, namely a CmmExpr to which an assignment can be
--- rewriten. We pass these two sets of facts - RegisterFacts and StackFacts -
--- to most of the functions as a tuple.
+-- We distinguish two kinds of facts: about assignments to registers and about
+-- assignments to stack locations. Former ones are associated with a value of
+-- CmmReg while the latter ones are associated with a tuple (Area, Int)
+-- representing a place on the stack. Since this pass is done before the stack
+-- layout, we don't yet have explict global registers like Sp and therefore we
+-- operate on stack areas with an offset (these are stored in a CmmStackSlot
+-- data constructor). Both types of facts take the same values, namely a CmmExpr
+-- to which an assignment can be rewriten. We pass these two sets of facts -
+-- RegisterFacts and StackFacts - to most of the functions as a tuple.
 
 -- Note [Assignment facts lattice]
 -- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -152,7 +143,7 @@ cpJoin lbl (OldFact (oldMem, oldReg)) (NewFact (newMem, newReg)) =
 --
 -- In the left branch we know that x is 4 an in the right one we know it is 3.
 -- This means that in the lowest block we are not allowed to rewrite x to any
--- value. We must denote that fact by marking value of x as Top. We do that
+-- value. We must denote that fact by marking value of x as NAC. We do that
 -- by removing entry about x from the set of facts (see Note [Assignment facts
 -- lattice]). A special case is joining with Bottom. Whenever we join a set of
 -- facts with Bottom we return that set as a result.
@@ -169,15 +160,23 @@ cpTransfer = mkFTransfer3 cpTransferFirst cpTransferMiddle cpTransferLast
 -- ~~~~~~~~~~~~~~~~~~~~~~~~
 --
 -- Transfer function is responsible for adding new facts to our set of facts.
--- We are only interested in facts about assignments:
---  * of a register or literal to a register. These facts result from the
---    appearence of CmmAssign nodes in the Cmm graph.
---  * of a register or literal to a stack slot. These facts result from the
---    appearence of CmmStore nodes in the Cmm graph.
--- These facts appear only in nodes that are open on entry and exit (see
--- Hoopl paper if you don't understand what this means). In other words
--- assignments don't appear in labels (entry nodes) and jumps, branches and
--- function calls (exit nodes).
+-- We are interested in the following facts:
+--  * assignment of a register or literal to a register. These facts result
+--    from the appearence of CmmAssign nodes in the Cmm graph.
+--  * assignment of a register or literal to a stack slot. These facts result
+--    from the appearence of CmmStore nodes in the Cmm graph. We only record
+--    facts about Old areas of the stack, as Young areas can be overwritten
+--    by a call.
+--  * every assignment to a register or stack location of a value other
+--    than literal or a register means that a given location is no longer
+--    a constant and we have to remove it from the list of known facts
+--
+-- We also have to take special care of the calls (CmmCall, CmmForeignCall and
+-- CmmUnsafeForeignCall), as each call kills a set of registers it may
+-- potentially overwrite. For normal calls these are all STG registers, while
+-- for foreign calls we only kill caller-saves registeres. List of registers
+-- being killed is actually based on implementation of instance declarations
+-- for DefinerOfRegs typeclass in CmmNode.
 
 cpTransferMiddle :: CmmNode O O -> CpFacts -> CpFacts
 cpTransferMiddle (CmmAssign lhs rhs@(CmmLit   _)) f =
@@ -189,7 +188,7 @@ cpTransferMiddle (CmmAssign lhs rhs@(CmmLit   _)) f =
 #endif
         where f' = addRegisterFact lhs rhs f
 cpTransferMiddle (CmmAssign lhs rhs@(CmmReg reg)) f =
-    if lhs /= reg  -- if we register facts like R1 = R1 then rewriting will go into an infinite loop
+    if lhs /= reg
     then
 #ifdef DEBUG
         trace ("\nAdding register fact: " ++ show lhs ++ " := " ++ show rhs ++
@@ -270,28 +269,46 @@ cpTransferLast n f = distributeFact n f
 --             Utility functions for adding and finding facts
 -----------------------------------------------------------------------------
 
--- Note [Adding new facts]
--- ~~~~~~~~~~~~~~~~~~~~~~~~
+-- Note [Adding and dropping facts]
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 --
--- We add new facts by inserting values into the map. In this way old values
--- (old facts) are simply replaced by new ones. `first` and `second` functions
--- are taken from Control.Arrows and help to avoid boilerplate related to
--- handling values inside a tuple (remember that CpFacts is a tuple).
+-- There is a bit of assymetry between facts about stack locations and facts
+-- about registers, which results from the fact that registers can appear either
+-- as a LHS and RHS in a fact representation, whereas stack locations appear
+-- only on the LHS. So when we add fact about a stack location we simply insert
+-- it to a map of known facts (if a fact about this location existed it is
+-- overwritten). Similarily for dropping a fact about stack location - we just
+-- delete it from a map and that's it. Register facts are a bit more tricky.
+-- Let's say we know that { R1 = R2 } and we encounter an assignment R2 := 3.
+-- This means that { R1 = R2 } is no longer true, because value of R2 has
+-- changed and is no longer the same as R1. So before adding a new fact about
+-- register we must kill all facts that have that register on RHS. An important
+-- invariant: if we have a fact about a register, that register cannot appear
+-- on a RHS of any other fact. Justification: consider we know {R1 = 3} and
+-- we encounter assignment R2:=R1. Before recording it as a fact we rewrite
+-- that assignment using know facts, which yields new assignment R2:=3, which
+-- we add as a fact. This gives us new set of facts {R1=3,R2=3} and the
+-- invariant is maintained. Conversely, if a register is on a RHS of at least
+-- one fact, we cannot have a fact containing that register on LHS.
 
 addStackFact :: StackLocation -> CpFact -> CpFacts -> CpFacts
 addStackFact _   _      (Bottom     ,     _) = panic "Adding stack facts to Bottom"
 addStackFact lhs rhs fs@(Const stkFs, regFs) = (Const $ M.insert lhs rhs stkFs, regFs)
 
 addRegisterFact :: RegisterLocation -> CpFact -> CpFacts -> CpFacts
-addRegisterFact lhs rhs fs@(Const stkFs, Const regFs) =
+addRegisterFact lhs rhs fs@(Const stkFs, Const regFs)
+    | Just _ <- lookupRegisterFact lhs fs = (Const stkFs, Const $ M.insert lhs rhs regFs)
+    | otherwise = let (stkFs', regFs') = killDef lhs (stkFs, regFs)
+                  in (Const stkFs', Const $ M.insert lhs rhs regFs')
+{-
     case lookupRegisterFact lhs fs of
-      Nothing   -> let (stkFs', regFs') = killDef lhs (stkFs, regFs) -- eliminate all facts where lhs register appears as rhs
-                   in (Const stkFs', Const $ M.insert lhs rhs regFs') -- and add new fact about register
+      Nothing   -> let (stkFs', regFs') = killDef lhs (stkFs, regFs)
+                   in (Const stkFs', Const $ M.insert lhs rhs regFs')
       Just fact -> if fact == rhs
                    then fs -- we already know that fact, nothing happens
-                   else let (stkFs', regFs') = killDef lhs (stkFs, regFs) -- eliminate all facts where lhs register appears as rhs
-                        in (Const stkFs', Const $ M.insert lhs rhs regFs') -- and add new fact about register
-
+                   else let (stkFs', regFs') = killDef lhs (stkFs, regFs)
+                        in (Const stkFs', Const $ M.insert lhs rhs regFs')
+-}
 addRegisterFact _   _   _ = panic "Adding register facts to Bottom"
 
 killDef :: RegisterLocation
@@ -331,18 +348,6 @@ lookupRegisterFact k = lookupCpFact k . snd
 lookupCpFact :: Ord a => a -> AssignmentFactBot a -> Maybe CpFact
 lookupCpFact k (Const m) = M.lookup k m
 lookupCpFact _  Bottom   = panic "Looking for facts in bottom"
-
-{-
-sumFacts :: CpFact -> CpFact -> CpFact
-sumFacts CpTop        _     = CpTop
-sumFacts _            CpTop = CpTop
-sumFacts (CpInfo e1) (CpInfo e2) | e1 == e2  = CpInfo e1
-                                 | otherwise = CpTop
-
-maybeCmmExpr :: CpFact -> Maybe CmmExpr
-maybeCmmExpr  CpTop        = Nothing
-maybeCmmExpr (CpInfo fact) = Just fact
--}
 
 -- See Note [Join operation for copy propagation]
 joinCpFacts :: (Show v, Ord v)
@@ -500,10 +505,10 @@ cpRwLast      (CmmForeignCall tgt res args succ ret_args ret_off intrbl) =
     rwForeignCall tgt res args (\t r a ->
       gUnitOC . (BlockOC BNil) . CmmForeignCall t r a succ ret_args ret_off $ intrbl)
 
-cpRwLast _ = const $ return Nothing
 cpRwLast call@(CmmCall { cml_target = target }) =
     rwCmmExprToGraphOC (\t -> call {cml_target = t}) target
 
+cpRwLast _ = const $ return Nothing
 
 -----------------------------------------------------------------------------
 --                 Utility functions for node rewriting
