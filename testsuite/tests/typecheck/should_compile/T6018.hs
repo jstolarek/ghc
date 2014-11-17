de9f666e55d57f9a835c343b6d14810ad0c3bbe6
Jan Stolarek <jan.stolarek@p.lodz.pl>
2014-11-17 10:54:27 +0100

    Name refactoring + comments

--- testsuite/tests/typecheck/should_compile/T6018.hs
+++ testsuite/tests/typecheck/should_compile/T6018.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TypeFamilies, DataKinds, UndecidableInstances #-}
+{-# LANGUAGE TypeFamilies, DataKinds, UndecidableInstances, PolyKinds #-}
 
 module T6018 where
 
@@ -12,14 +12,14 @@ type instance F Bool Int  Char = Char
 
 type instance G Bool Int  Char = Char
 
-type family I a b c = r | r -> a b
+type family I (a :: *) b (c :: *) = r | r -> a b
 type instance I Int  Char Bool = Bool
 type instance I Int  Char Int  = Bool
 type instance I Bool Int  Int  = Int
 
 -- this is injective - a type variables mentioned on LHS is not mentioned on RHS
 -- but we don't claim injectivity in that argument.
-type family J a b = r | r -> a
+type family J a (b :: k) = r | r -> a
 type instance J Int b = Char
 
 type MaybeSyn a = Maybe a
