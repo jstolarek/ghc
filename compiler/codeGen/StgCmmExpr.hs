649373dbd9d8b45564d51b006df60400a6d8f90d
Jan Stolarek <jan.stolarek@p.lodz.pl>
2014-11-28 13:35:29 +0100

    Remove unused paremeter

--- compiler/codeGen/StgCmmExpr.hs
+++ compiler/codeGen/StgCmmExpr.hs
@@ -345,10 +345,10 @@ cgCase (StgApp v []) bndr alt_type@(PrimAlt _) alts
        ; v_info <- getCgIdInfo v
        ; emitAssign (CmmLocal (idToReg dflags (NonVoid bndr))) (idInfoToAmode v_info)
        ; _ <- bindArgsToRegs [NonVoid bndr]
-       ; (maybeRetKind, maybeAltCons, precompStates, maybeHpOffsets) <-
+       ; (maybeRetKind, maybeAltCons, precompStates, _) <-
            preCgAlts (NonVoid bndr) alt_type alts
        ; cgAlts (NoGcInAlts, AssignedDirectly) (NonVoid bndr)
-                alt_type maybeRetKind maybeAltCons precompStates maybeHpOffsets
+                alt_type maybeRetKind maybeAltCons precompStates
        }
   where
     reps_compatible = idPrimRep v == idPrimRep bndr
@@ -409,7 +409,7 @@ cgCase scrut bndr alt_type alts
        ; ret_kind <- withSequel sequel (cgExpr scrut)
        ; restoreCurrentCostCentre mb_cc
        ; cgAlts (gc_plan, ret_kind) (NonVoid bndr)
-                alt_type maybeRetKind maybeAltCons precompStates maybeHpOffsets
+                alt_type maybeRetKind maybeAltCons precompStates
        }
 
 
@@ -519,22 +519,21 @@ cgAlts :: (GcPlan,ReturnKind)
        -> Maybe ReturnKind
        -> Maybe [AltCon]
        -> [CgState]
-       -> Maybe [VirtualHpOffset]
        -> FCode ReturnKind
 -- At this point the result of the case are in the binders
-cgAlts gc_plan _bndr PolyAlt (Just retKind) Nothing [state] Nothing
+cgAlts gc_plan _bndr PolyAlt (Just retKind) Nothing [state]
   = do maybeAltHeapCheck gc_plan state
        return retKind
 
-cgAlts gc_plan _bndr (UbxTupAlt _) (Just retKind) Nothing [state] Nothing
+cgAlts gc_plan _bndr (UbxTupAlt _) (Just retKind) Nothing [state]
   = do maybeAltHeapCheck gc_plan state
        return retKind
         -- Here bndrs are *already* in scope, so don't rebind them
 
-cgAlts gc_plan bndr (PrimAlt _) Nothing (Just altCons) states (Just hpOffsets)
+cgAlts gc_plan bndr (PrimAlt _) Nothing (Just altCons) states
   = do  { dflags <- getDynFlags
 
-        ; altsWithHeapChecks <- cgAltRhss gc_plan states hpOffsets
+        ; altsWithHeapChecks <- cgAltRhss gc_plan states
 
         ; let tagged_cmms = zip altCons altsWithHeapChecks
               bndr_reg = CmmLocal (idToReg dflags bndr)
@@ -548,10 +547,9 @@ cgAlts gc_plan bndr (PrimAlt _) Nothing (Just altCons) states (Just hpOffsets)
         ; return AssignedDirectly }
 
 cgAlts gc_plan bndr (AlgAlt tycon) Nothing (Just altCons) states
-       (Just hpOffsets)
   = do  { dflags <- getDynFlags
 
-        ; (mb_deflt, branches) <- cgAlgAltRhss gc_plan altCons states hpOffsets
+        ; (mb_deflt, branches) <- cgAlgAltRhss gc_plan altCons states
 
         ; let fam_sz   = tyConFamilySize tycon
               bndr_reg = CmmLocal (idToReg dflags bndr)
@@ -574,7 +572,7 @@ cgAlts gc_plan bndr (AlgAlt tycon) Nothing (Just altCons) states
                    emitSwitch tag_expr branches mb_deflt 0 (fam_sz - 1)
                    return AssignedDirectly }
 
-cgAlts _ _ _ _ _ _ _ = panic "cgAlts"
+cgAlts _ _ _ _ _ _ = panic "cgAlts"
         -- UbxTupAlt and PolyAlt have only one alternative
 
 
@@ -600,11 +598,10 @@ cgAlts _ _ _ _ _ _ _ = panic "cgAlts"
 
 -------------------
 cgAlgAltRhss :: (GcPlan,ReturnKind) -> [AltCon] -> [CgState]
-             -> [VirtualHpOffset]
              -> FCode ( Maybe CmmAGraph
                       , [(ConTagZ, CmmAGraph)] )
-cgAlgAltRhss gc_plan altCons states hpOffsets
-  = do { altsWithHeapChecks <- cgAltRhss gc_plan states hpOffsets
+cgAlgAltRhss gc_plan altCons states
+  = do { altsWithHeapChecks <- cgAltRhss gc_plan states
 
        ; let tagged_cmms = zip altCons altsWithHeapChecks
              mb_deflt = case tagged_cmms of
@@ -620,9 +617,8 @@ cgAlgAltRhss gc_plan altCons states hpOffsets
 
 
 -------------------
-cgAltRhss :: (GcPlan,ReturnKind) -> [CgState] -> [VirtualHpOffset]
-          -> FCode [CmmAGraph]
-cgAltRhss gc_plan states hpOffsets = do
+cgAltRhss :: (GcPlan,ReturnKind) -> [CgState] -> FCode [CmmAGraph]
+cgAltRhss gc_plan states = do
   let
     cg_alt :: CgState -> FCode CmmAGraph
     cg_alt altState
