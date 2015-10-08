f56b8402ec42efbcff681ac80ce0d897f60f466b
Jan Stolarek <jan.stolarek@p.lodz.pl>
2015-10-08 09:26:50 +0200

    Comments only

--- compiler/hsSyn/HsExpr.hs
+++ compiler/hsSyn/HsExpr.hs
@@ -1668,7 +1668,7 @@ pprQuals quals = interpp'SP quals
 -}
 
 data HsSplice id
-   = HsTypedSplice       --  $z  or $(f 4)
+   = HsTypedSplice       --  $$z  or $$(f 4)
         id               -- A unique name to identify this splice point
         (LHsExpr id)     -- See Note [Pending Splices]
 
