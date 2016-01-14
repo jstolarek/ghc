511e073c7ccd7e897a264bfa22e9b9dcc80ea04c
Jan Stolarek <jan.stolarek@p.lodz.pl>
2016-01-14 17:25:01 +0100

    Add test

---
+++ testsuite/tests/typecheck/should_compile/T11080.hs
@@ -0,0 +1,33 @@
+-- JSTOLAREK: some language extension here
+{-# LANGUAGE TypeInType #-}
+module T11080 where
+
+import Data.Kind
+
+-- closed kind using H98 syntax
+data kind Universe = Sum  Universe Universe
+                   | Prod Universe Universe
+                   | K (*)
+
+-- JSTOLAREK: this should be rejected!
+foo :: Universe
+foo = Sum undefined undefined
+{-
+-- closed kind using GADTs syntax
+data kind Universe where
+    Sum  :: Universe -> Universe -> Universe
+    Prod :: Universe -> Universe -> Universe
+    K    :: *                    -> Universe
+-}
+
+{-
+Things to test:
+
+  * open data kinds declaration
+  * make sure that type-level usage works
+  * make sure that term-level use is rejected
+  * make sure that things work between modules (interface files implemented
+    correctly)
+  * Check that indexing works
+-}
+
